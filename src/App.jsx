20b3aa80a1c90d7f643ce88448ec65fc963c8a85
tasnimlima07 <tasnimlima07@gmail.com>
2024-04-15 23:07:46 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Button } from '@mui/material'
+
+const App = () => {
+  return (
+    <div>
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
+    </div>
+  )
+}
+
+export default App
