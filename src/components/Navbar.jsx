bc33912bcbadf78768c34e1b8ad89d9f960a9310
tasnimlima07 <tasnimlima07@gmail.com>
2024-04-16 18:34:07 +0600

    'components'

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,11 @@
-import { AppBar, Icon, Toolbar, Typography, styled, Box } from '@mui/material'
+import { AppBar, Icon, Toolbar, Typography, styled, Box, Avatar } from '@mui/material'
 import React from 'react'
 import AcUnitIcon from '@mui/icons-material/AcUnit';
 import InputBase from '@mui/material/InputBase';
-
+import Badge from '@mui/material/Badge';
+import MailIcon from '@mui/icons-material/Mail';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import { deepOrange, deepPurple } from '@mui/material/colors';
 
 const StyledToolbar = styled(Toolbar)({
   display: "flex",
@@ -18,7 +21,9 @@ const Search = styled("div")(({theme}) => ({
 }));
 
 const Icons = styled(Box)(({theme}) => ({
-  background: "white",
+  display: "flex",
+  gap: "20px",
+  alignItems: "center",
 }));
 
 const Navbar = () => {
@@ -28,7 +33,16 @@ const Navbar = () => {
         <Typography variant="h6" sx={{ display: { xs: "none", sm: "block"}}}>Navbar</Typography>
         <AcUnitIcon sx={{ display: { xs: "block", sm: "none"}}}/>
         <Search><InputBase placeholder='search...'/></Search>
-        <Icons>Icons</Icons>
+        <Icons>
+          <Badge badgeContent={4} color="error">
+           <MailIcon color="white"/>
+          </Badge>
+          <Badge badgeContent={4} color="error">
+           <NotificationsIcon color="white"/>
+          </Badge>
+          <Avatar sx={{ width: 30, height: 30, bgcolor: deepOrange[500] }}>N</Avatar>
+
+        </Icons>
       </StyledToolbar>
       
     </AppBar>
